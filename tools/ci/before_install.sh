a57db1d1a4ac47663a597e803a39e036b3c1718d
Alberto Jesus Gutierrez Juanes <aljesusg@gmail.com>
2016-08-06 12:15:25 +0200

    Init App

---
+++ tools/ci/before_install.sh
@@ -0,0 +1,15 @@
+set -v
+
+echo "gem: --no-ri --no-rdoc --no-document" > ~/.gemrc
+travis_retry gem install bundler -v ">= 1.11.1"
+
+if [[ -n "${GEM}" ]] ; then
+  cd gems/${GEM}
+else
+  cp config/database.pg.yml config/database.yml
+  psql -c "CREATE USER meetcoders SUPERUSER PASSWORD 'smartcoder';" -U postgres
+  export BUNDLE_WITHOUT=development
+fi
+export BUNDLE_GEMFILE=${PWD}/Gemfile
+
+set +v
